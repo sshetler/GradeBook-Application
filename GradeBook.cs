4c09c7401326823c9a8775916da2365a1d9c69a1
sshetler <shetlergs@gmail.com>
2026-03-19 13:20:48 -0400

    Add files via upload

---
+++ GradeBook.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class GradeBook
+{
+    public string CourseName { get; set; }//auto-implemented properties
+
+    public int NumOfCredits { get; set; }
+
+    public string CourseNum { get; set; }
+
+    public GradeBook(string cName, int credits, string cNum)//constructor
+    {
+        CourseName = cName;
+        NumOfCredits = credits;
+        CourseNum = cNum;
+    }
+    public decimal CalcAverage()//property method
+    {
+        decimal total = 0;//local variable
+
+        int count = 1;
+        while(count <= 5)
+        {
+            Console.Write("Please enter a grade: ");
+            int grade = int.Parse(Console.ReadLine());
+
+            total = total + grade;
+
+            count = count + 1;
+        }
+        decimal avg = total / (count - 1);//local variable
+
+        return avg;
+    }
+}
---
+++ GradeBookTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+public class GradeBookTest
+{
+    public static void Main()
+    {
+        Console.Write("Please enter the course name: ");
+        string cName = Console.ReadLine();
+
+        Console.Write("Please enter the number of credits for the course: ");
+        int credits = int.Parse(Console.ReadLine());
+
+        Console.Write("Please enter the course number: ");
+        string cNum = Console.ReadLine();
+
+        GradeBook mygb = new GradeBook(cName, credits, cNum);//header variables
+
+        decimal avg = mygb.CalcAverage();
+
+        Console.WriteLine("The average of the 5 students is: " + avg);
+
+    }
+}
